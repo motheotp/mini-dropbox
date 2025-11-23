40c11654c415bf909ee5c8e9c24f6a7fd893a145
Motheo Treasure Puso <motheotp@gmail.com>
2025-11-23 09:26:54 -0600

    upload and delete being replicated amongst nodes accordingly. passes partition tests.

--- arch2/services/download/app.py
+++ arch2/services/download/app.py
@@ -7,7 +7,9 @@ import requests, os
 
 app = Flask(__name__)
 
-METADATA_API = "http://metadata:5005" # metadata service URL
+# METADATA_API = "http://metadata:5005" # metadata service URL
+
+METADATA_API = os.environ.get("METADATA_API", "http://metadata-gateway:5005")
 STORAGE_API = "http://storage:5006" # storage service URL
 SECRET_KEY = os.environ.get("SECRET_KEY", "supersecretkey") # secret key for JWT - in more secure setup, use env variable
 
